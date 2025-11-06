f3b8d99c5bdb100a04b7a4a06c6ce94fbfd72ca3
Leonel Fonseca <n.fonsecao@uniandes.edu.co>
2025-11-06 11:59:44 -0500

    fix(ecs): adjust ECS service deployment health parameters

--- infra/terraform/modules/optimizador_rutas/main.tf
+++ infra/terraform/modules/optimizador_rutas/main.tf
@@ -291,8 +291,8 @@ resource "aws_ecs_service" "this" {
     container_port   = var.app_port
   }
 
-  deployment_minimum_healthy_percent = 50
-  deployment_maximum_percent         = 200
+  deployment_minimum_healthy_percent = 0
+  deployment_maximum_percent         = 100
 
   depends_on = [aws_lb_listener_rule.optimizer_path]
 
